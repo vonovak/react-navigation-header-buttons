3dd68f7bf2a92e4b37a40c7dc4798e6bff5c6a1c
Vojtech Novak <vonovak@gmail.com>
2020-10-18 12:33:19 +0200

    remove custom ripple demo

--- example/screens/index.ts
+++ example/screens/index.ts
@@ -3,7 +3,6 @@ export { UsageWithIcons } from './UsageWithIcons';
 export { UsageWithOverflow } from './UsageWithOverflow';
 export { UsageLeft } from './UsageLeft';
 export { UsageDisabled } from './UsageDisabled';
-export { UsageCustomRipple } from './UsageCustomRipple';
 export { UsageDifferentFontFamilies } from './UsageDifferentFontFamilies';
 export { HomeScreen } from './HomeScreen';
 export { UsageWithCustomOverflow } from './UsageCustomOverflow';
