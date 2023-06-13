867b9a279cc9905f89431a9f8eaa0488ef571492
Vojtech Novak <vonovak@gmail.com>
2023-06-13 11:59:05 +0200

    chore: clean jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  // modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
-  // transform: {
-  //   '^.+\\.js|.tsx$': require.resolve('react-native/jest/preprocessor.js'),
-  // },
-  // setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   modulePathIgnorePatterns: [
     '<rootDir>/example/node_modules',
     '<rootDir>/lib/',
@@ -12,9 +7,7 @@ module.exports = {
   maxWorkers: 2,
   coverageProvider: 'v8',
   moduleNameMapper: {
-    // '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
     '^[./a-zA-Z0-9$_-]+\\.png$':
       '<rootDir>/node_modules/react-native/Libraries/Image/RelativeImageStub',
-    // 'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
   },
 };
