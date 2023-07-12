c38b82c920adb0f2e34ebe6e2cfafda4a931a14c
Vojtech Novak <vonovak@gmail.com>
2023-07-12 12:03:30 +0200

    chore: move lint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {},
+  ignorePatterns: ['lib/*', 'node_modules/*'],
+};
